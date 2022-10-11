5e5fee099f257ac9ef01d716f3660e23fb134481
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-11 20:41:14 +0800

    mvc

--- src/main/java/spring/mvc/session08/controller/HelloController.java
+++ src/main/java/spring/mvc/session08/controller/HelloController.java
@@ -2,14 +2,14 @@ package spring.mvc.session08.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
 	/* 
-	 * 1.
-	 * 執行路徑: /mvc/hello/welcome
+	 * 1. 執行路徑: /mvc/hello/welcome
 	 * /mvc 在 web.xml 中有定義
 	 * /hello 找到 HelloController
 	 * /welcome 執行 welcome() 方法
@@ -19,4 +19,23 @@ public class HelloController {
 	public String welcome() {
 		return "Welcome SpringMVC !";
 	}
+	
+	/*
+	 * 2. ?後面帶參數 @RequestParam
+	 * 執行路徑: /mvc/hello/sayhi?name=John&age=18
+	 * */
+	@RequestMapping("/sayhi")
+	@ResponseBody
+	public String sayHi(@RequestParam(value = "name") String name,
+						@RequestParam(value = "age") Integer age) {
+		return String.format("Hi %s %d", name, age);
+	}
+	
 }
+
+
+
+
+
+
+
