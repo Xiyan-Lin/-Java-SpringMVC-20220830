4e4da0651a360736da3a2ddb409aa15c5c4c16b0
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-13 21:06:27 +0800

    mvc

--- src/main/java/spring/mvc/session08/controller/DataController.java
+++ src/main/java/spring/mvc/session08/controller/DataController.java
@@ -15,14 +15,23 @@ public class DataController {
 	//@GetMapping(value = "/case1")
 	@GetMapping("/case1")
 	public ModelAndView case1() {
-		String data = "Hello data"; // 資料(Model)
+		String data = "Hello data 1"; // 資料(Model)
 		String view = "/WEB-INF/view/session08/show_data.jsp"; // 資料呈現地(資料渲染地)
 		// 建立 ModelAndView, 目的: 將 data(model) 與 view 封裝起來
 		ModelAndView mv = new ModelAndView();
-		mv.addObject("data2", data);
+		mv.addObject("data1", data);
 		mv.setViewName(view);
 		return mv;
 	}
+	
+	@GetMapping("/case2")
+	public ModelAndView case2() {
+		String data = "Hello data 2"; // 資料(Model)
+		String view = "/WEB-INF/view/session08/show_data.jsp"; // 資料呈現地(資料渲染地)
+		// 建立 ModelAndView, 目的: 將 data(model) 與 view 封裝起來
+		return new ModelAndView(view, "data2", data);
+	}
+	
 	
 	
 }
