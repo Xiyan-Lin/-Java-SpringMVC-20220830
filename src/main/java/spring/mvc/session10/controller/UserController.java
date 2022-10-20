e4b0caa3fd6b7c0184732edc24c7dd810738454c
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-20 19:04:23 +0800

    user

--- src/main/java/spring/mvc/session10/controller/UserController.java
+++ src/main/java/spring/mvc/session10/controller/UserController.java
@@ -1,5 +1,35 @@
 package spring.mvc.session10.controller;
 
-public class UserController {
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import spring.mvc.session10.entity.User;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	private List<User> users = new CopyOnWriteArrayList<>(); // In-memory db
+	
+	// 建構初始時作區
+	{
+		users.add(new User("John", 20, new Date(), "大學", "男", new String[] {"看電影", "玩遙控", "寫程式"}, "履歷表一"));
+		users.add(new User("Mary", 23, new Date(), "研究所", "女", new String[] {"爬山"}, "履歷表二"));
+		users.add(new User("Bobo", 16, new Date(), "高中", "男", new String[] {"看電影", "寫程式"}, "履歷表三"));
+	}
+	
+	public String index(Model model, @ModelAttribute User user) {
+		// 有 @ModelAttribute User user 在方法中定義, 所以下面二行可以不用寫
+		//User user = new User();
+		//model.addAttribute("user", user);
+		
+		return "session10/user";
+	}
+	
 }
