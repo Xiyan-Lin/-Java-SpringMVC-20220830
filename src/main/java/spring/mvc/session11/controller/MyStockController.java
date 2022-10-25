e8ac1e5bd2017dbecfbe756e79ba0ea8ad066e51
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-25 20:53:33 +0800

    mystock

--- src/main/java/spring/mvc/session11/controller/MyStockController.java
+++ src/main/java/spring/mvc/session11/controller/MyStockController.java
@@ -3,10 +3,14 @@ package spring.mvc.session11.controller;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import spring.mvc.session11.entity.MyStock;
@@ -22,4 +26,13 @@ public class MyStockController {
 		return "session11/mystock";
 	}
 	
+	@PostMapping("/")
+	public String add(Model model, @ModelAttribute @Valid MyStock myStock, BindingResult result) {
+		if(result.hasErrors()) {
+			model.addAttribute("myStocks", myStocks);
+			return "session11/mystock";
+		}
+		myStocks.add(myStock);
+		return "redirect:./";
+	}
 }
