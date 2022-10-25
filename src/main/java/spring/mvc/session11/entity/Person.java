8365fe2b47469ad6e26b123b2ae27483e2a28bea
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-25 19:00:52 +0800

    person

--- src/main/java/spring/mvc/session11/entity/Person.java
+++ src/main/java/spring/mvc/session11/entity/Person.java
@@ -2,16 +2,35 @@ package spring.mvc.session11.entity;
 
 import java.util.Date;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class Person {
 	
+	@NotEmpty(message = "姓名不可是空值")
+	@Size(min = 2, max = 50, message = "名字範圍必須介於2~50個字之間")
 	private String name; // 姓名
 	
+	@NotNull(message = "年齡不可是空值")
+	@Range(min = 18, max = 120, message = "年齡必須介於18~120歲之間")
 	private Integer age; // 年齡
 	
+	@NotNull(message = "會員設定不可是空值")
 	private Boolean member; // 是否是會員
 	
+	@NotNull(message = "生日不可是空值")
+	@Past(message = "生日不可大於現在的日期")
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date birth; // 生日
-
+	
 	public String getName() {
 		return name;
 	}
