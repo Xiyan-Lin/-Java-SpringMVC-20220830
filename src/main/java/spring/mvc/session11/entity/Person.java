4203112cadcea627eb7c753e36bbb6aa0f3bb0bc
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-25 18:53:55 +0800

    Person 會員資料

---
+++ src/main/java/spring/mvc/session11/entity/Person.java
@@ -0,0 +1,53 @@
+package spring.mvc.session11.entity;
+
+import java.util.Date;
+
+public class Person {
+	
+	private String name; // 姓名
+	
+	private Integer age; // 年齡
+	
+	private Boolean member; // 是否是會員
+	
+	private Date birth; // 生日
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Boolean getMember() {
+		return member;
+	}
+
+	public void setMember(Boolean member) {
+		this.member = member;
+	}
+
+	public Date getBirth() {
+		return birth;
+	}
+
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", member=" + member + ", birth=" + birth + "]";
+	}
+	
+	
+}
