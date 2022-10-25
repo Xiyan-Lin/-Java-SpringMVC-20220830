96fda6d0d3cb46e67efa3934c4b9630ab892267e
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-25 19:28:18 +0800

    person

--- src/main/java/spring/mvc/session11/controller/PersonController.java
+++ src/main/java/spring/mvc/session11/controller/PersonController.java
@@ -3,10 +3,14 @@ package spring.mvc.session11.controller;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import spring.mvc.session11.entity.Person;
@@ -22,5 +26,18 @@ public class PersonController {
 		model.addAttribute("people", people);
 		return "session11/person";
 	}
+	
+	@PostMapping("/")
+	public String add(@ModelAttribute @Valid Person person, BindingResult result) {
+		// 驗證 Person
+		// 驗證結果會放在 result 中
+		if(result.hasErrors()) {
+			// 若有錯誤發生就會自動機錯誤訊息傳送到指定的 view 中
+			return "session11/person";
+		}
+		people.add(person);
+		return "redirect:./";
+	}
+	
 	
 }
