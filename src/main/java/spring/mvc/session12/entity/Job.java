3f363a77387a3b1363336203416a66f019454ba0
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-27 19:16:43 +0800

    jdbc

--- src/main/java/spring/mvc/session12/entity/Job.java
+++ src/main/java/spring/mvc/session12/entity/Job.java
@@ -3,8 +3,57 @@ package spring.mvc.session12.entity;
 import java.util.Date;
 import java.util.List;
 
+import javax.validation.constraints.Size;
+
 public class Job {
 	
-	private Integer 
+	private Integer jid; // 資料表序號
+	
+	@Size(min = 2, max = 50, message = "{job.jname.size}")
+	private String jname;
+	
+	private Integer eid;
+	
+	// 一個工作最多可以配置一個員工
+	private Employee employee;
+
+	public Integer getJid() {
+		return jid;
+	}
+
+	public void setJid(Integer jid) {
+		this.jid = jid;
+	}
+
+	public String getJname() {
+		return jname;
+	}
+
+	public void setJname(String jname) {
+		this.jname = jname;
+	}
+
+	public Integer getEid() {
+		return eid;
+	}
+
+	public void setEid(Integer eid) {
+		this.eid = eid;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	@Override
+	public String toString() {
+		return "Job [jid=" + jid + ", jname=" + jname + ", eid=" + eid + ", employee=" + ((employee==null)?null:employee.getEname()) + "]";
+	}
+	
+	
 	
 }
