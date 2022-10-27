69afe8c11aec552fc196e29fa7323dae049086d0
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-27 20:49:32 +0800

    jdbc

--- src/main/java/spring/mvc/session12/controller/EmployeeController.java
+++ src/main/java/spring/mvc/session12/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package spring.mvc.session12.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import spring.mvc.session12.entity.Employee;
@@ -23,4 +27,16 @@ public class EmployeeController {
 		model.addAttribute("employees", employeeDao.query());
 		return "session12/employee";
 	}
+	
+	@PostMapping("/")
+	public String add(@ModelAttribute @Valid Employee employee, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("_method", "POST");
+			model.addAttribute("employees", employeeDao.query());
+			return "session12/employee"; 
+		}
+		employeeDao.add(employee);
+		return "redirect:./";
+	}
+	
 }
