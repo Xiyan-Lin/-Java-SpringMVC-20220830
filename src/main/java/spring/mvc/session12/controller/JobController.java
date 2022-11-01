33657999bb2f64621f78296640d4d4fe8030d396
MB-teacher <MB-teacher@MB-207-teacher>
2022-11-01 19:30:42 +0800

    jdbc

--- src/main/java/spring/mvc/session12/controller/JobController.java
+++ src/main/java/spring/mvc/session12/controller/JobController.java
@@ -1,10 +1,14 @@
 package spring.mvc.session12.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import spring.mvc.session12.entity.Job;
@@ -21,7 +25,6 @@ public class JobController {
 	@Autowired
 	private EmployeeDao employeeDao;
 	
-	
 	@GetMapping("/")
 	public String index(@ModelAttribute Job job, Model model) {
 		model.addAttribute("_method", "POST");
@@ -29,4 +32,18 @@ public class JobController {
 		model.addAttribute("employees", employeeDao.query());
 		return "session12/job";
 	}
+	
+	@PostMapping("/")
+	public String add(@ModelAttribute @Valid Job job, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("_method", "POST");
+			model.addAttribute("jobs", jobDao.query());
+			model.addAttribute("employees", employeeDao.query());
+			return "session12/job";
+		}
+		jobDao.add(job);
+		return "redirect:./";
+	}
+	
+	
 }
