3e9f0ebb06d32c11b04ba5020658d892076c8740
MB-teacher <MB-teacher@MB-207-teacher>
2022-11-08 18:53:44 +0800

    exception

--- src/main/java/spring/mvc/session13/controller/DivController.java
+++ src/main/java/spring/mvc/session13/controller/DivController.java
@@ -1,4 +1,4 @@
-package spring.mvc.session13;
+package spring.mvc.session13.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
